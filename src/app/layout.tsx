72f2a69c7bb510403f9501ef132395a044f374b1
Roby Codes <robert.cfx@proton.me>
2024-05-21 03:30:27 +0200

    seo(favicon): Updated favicon
    Updated favicon and app title for better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tic Tac Toe - Have Fun Now!",
+  title: "Tic Tac Toe - Challenge Your Friends Now!",
   description:
     "A fun and engaging Tic Tac Toe web application to enjoy with your friends, built using Next.js and other modern technologies for a seamless and interactive experience.",
 };
