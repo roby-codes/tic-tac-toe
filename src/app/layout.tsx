9edb6d131184a0e12b958c3c8613fcc47f49f299
Roby Codes <robert.cfx@proton.me>
2024-05-22 22:59:32 +0200

    style(tailwind-css): Added prettierrc plugin to sort tailwind-css classes efficently

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "~/utils/cn";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, `antialiased`)}>{children}</body>
     </html>
   );
 }
