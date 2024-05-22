64e0f2546e1c2917c0592e3c0cbe03e6e1f2fab7
Roby Codes <robert.cfx@proton.me>
2024-05-22 22:52:34 +0200

    feat(cn): Added cn utility for better tailwind class management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,11 @@
-"use client";
+import { cn } from "~/utils/cn";
 
-import { useEffect, useState } from "react";
-
-const HomePage = () => {
-  const [text, setText] = useState("post loading");
-  const [id, setId] = useState(1);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    fetch(`https://dummyjson.com/products/${id}`, {
-      signal: controller.signal,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setText(data.description);
-      });
-
-    return () => controller.abort("");
-  }, [id]);
-
-  return (
-    <main>
-      <section className="min-h-[100dvh] flex justify-center items-center">
-        <div className="container flex flex-col items-center gap-4">
-          <p>{text}</p>
-          <button
-            className="bg-black hover:bg-black/50 text-white p-2"
-            onClick={() => setId((id) => id + 1)}
-          >
-            change post
-          </button>
-        </div>
-      </section>
-    </main>
-  );
-};
+const HomePage = () => (
+  <main>
+    <section className={cn("min-h-[100dvh]")}>
+      <h1>Tic Tac Toe</h1>
+    </section>
+  </main>
+);
 
 export default HomePage;
